56f5746b2c785cb0044eabd71398e307d17dfba1
cjredmond <connor.redmond@gmail.com>
2017-01-20 14:23:40 -0500

    added working pages with app

--- blank/blank/urls.py
+++ blank/blank/urls.py
@@ -1,8 +1,11 @@
 
 from django.conf.urls import url
 from django.contrib import admin
-#from test_app.views import PostDetailView, PostListView, PostCreateView
+from test_app.views import PostDetailView, PostListView, PostCreateView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-                                                ]
+	url(r'^post/(?P<pk>\d+)$', PostDetailView.as_view(), name="post_detail_view"),
+	url(r'^post/new/$', PostCreateView.as_view(), name="post_create_view"),
+	url(r'^posts/$', PostListView.as_view(), name="post_list_view")
+]
--- blank/test_app/models.py
+++ blank/test_app/models.py
@@ -1,1 +1,4 @@
 from django.db import models
+class Post(models.Model):
+	title = models.CharField(max_length=200)
+	content = models.CharField(max_length=200)
